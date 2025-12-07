eecb0c602165958303196f029c8e7903b29da45d
munchen777.eth <isak.georgiy@mail.ru>
2025-12-07 09:41:30 +0300

    add 3432 daily leetcode golang

--- 3432_count_partitions_with_even_sum_diff/main.go
+++ 3432_count_partitions_with_even_sum_diff/main.go
@@ -1,3 +1,24 @@
 package main
 
+func countPartitions(nums []int) int {
+    var result, rightSum, leftSum int
 
+    // count total sum for right part
+    for _, value := range nums {
+        rightSum += value
+    }
+
+    for i := 0; i < len(nums) - 1; i++ {
+        leftSum += nums[i]
+        rightSum -= nums[i]
+        // check if difference between left and right is even
+        if ((leftSum - rightSum) % 2) == 0 {
+            result++
+        }
+    }
+    return result
+}
+
+func main() {
+	countPartitions([]int{10, 10, 3, 7, 6})
+}
